9762a9191aa5c4ddc3f07d9c56db8fdca35c4a85
Половых Александр <Alexandr-polovykh@yandex.ru>
2016-07-11 04:03:31 +0600

    The currencies rates refreshing by timer is ready

--- TestWebApp2/Backend/CurrencyManager.cs
+++ TestWebApp2/Backend/CurrencyManager.cs
@@ -1,9 +1,107 @@
-﻿namespace TestWebApp2.Backend
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Xml;
+using System.Xml.XPath;
+using TestWebApp2.Backend.Entities;
+
+namespace TestWebApp2.Backend
 {
-	public class CurrencyManager
+	public static class CurrencyManager
 	{
-		public CurrencyManager()
+		public static void Initialize(AppContext context)
 		{
-		}		
+			var currencies = GetCurrencies(context);
+			var currentDateTime = GetCurrencyRates(context, currencies);
+			var timer = new Timer((s) =>
+			                      {
+				                      var state = (State) s;
+				                      state.LastUpdateTime = GetCurrencyRates(new AppContext(), state.Currencies,
+				                                                              state.LastUpdateTime);
+			                      },
+			                      new State()
+			                      {
+				                      Currencies = currencies,
+				                      LastUpdateTime = currentDateTime
+			                      },
+			                      new DateTime(0, 0, 1).Millisecond,
+			                      new DateTime(0, 0, 1).Millisecond);
+		}
+
+		#region Main functions
+
+		private static ICollection<Currency> GetCurrencies(AppContext context)
+		{
+			var doc = new XmlDocument();
+			doc.Load("http://www.cbr.ru/scripts/XML_val.asp?d=0");
+			var nodes = doc.SelectNodes("//Item");
+			if (null == nodes)
+			{
+				throw new XPathException();
+			}
+
+			var supportedCurrencies = ConfigurationManager.AppSettings["supported_currencies"].Split(';');
+			var currencies = (from node in nodes.Cast<XmlNode>()
+			                  let currencyName = node["EngName"]?.InnerText
+			                  where supportedCurrencies.Contains(currencyName)
+			                  let currencyId = node["ParentCode"]?.InnerText.Trim()
+			                  select new Currency()
+			                         {
+				                         ID = currencyId,
+				                         Name = currencyName
+			                         }).ToList();
+
+			context.Currencies.AddRange(currencies);
+			context.SaveChanges();
+
+			return currencies;
+		}
+
+		private static DateTime GetCurrencyRates(AppContext context, IEnumerable<Currency> currencies,
+		                                         DateTime? startsWith = null)
+		{
+			var currentDateTime = DateTime.Now;
+			var date1 = (startsWith ?? new DateTime(2016, 4, 1)).ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
+			var date2 = currentDateTime.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+			foreach (var currency in currencies)
+			{
+				var doc = new XmlDocument();
+				doc.Load($"http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1={date1}&date_req2={date2}&VAL_NM_RQ={currency.ID}");
+				var nodes = doc.SelectNodes("//Record");
+				if (null == nodes)
+				{
+					throw new XPathException();
+				}
+
+				context.CurrencyRates.AddRange(from node in nodes.Cast<XmlNode>()
+				                               let date = DateTime.Parse(node?.Attributes?["Date"].Value)
+				                               let nominal = int.Parse(node["Nominal"]?.InnerText ?? "1")
+				                               let value = node["Value"]?.InnerText
+				                               where null != value
+				                               select new CurrencyRate()
+				                                      {
+					                                      CurrencyID = currency.ID,
+					                                      Currency = currency,
+					                                      Date = date,
+					                                      Value = decimal.Parse(value)/nominal
+				                                      });
+			}
+
+			context.SaveChanges();
+
+			return currentDateTime;
+		}
+
+		#endregion
+
+		private class State
+		{
+			internal ICollection<Currency> Currencies { get; set; }
+			internal DateTime? LastUpdateTime { get; set; }
+		}
 	}
 }
--- TestWebApp2/Backend/DbInitializer.cs
+++ TestWebApp2/Backend/DbInitializer.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data.Entity;
-using System.Globalization;
-using System.Linq;
-using System.Xml;
-using System.Xml.XPath;
-using TestWebApp2.Backend.Entities;
+﻿using System.Data.Entity;
 
 namespace TestWebApp2.Backend
 {
@@ -14,69 +6,9 @@ namespace TestWebApp2.Backend
 	{
 		protected override void Seed(AppContext context)
 		{
-			var currencies = InitializeCurrencies(context);
-			InitializeCurrencyRates(context, currencies);
+			CurrencyManager.Initialize(context);
 
 			base.Seed(context);
-		}
-
-		private static IEnumerable<Currency> InitializeCurrencies(AppContext context)
-		{
-			var doc = new XmlDocument();
-			doc.Load("http://www.cbr.ru/scripts/XML_val.asp?d=0");
-			var nodes = doc.SelectNodes("//Item");
-			if (null == nodes)
-			{
-				throw new XPathException();
-			}
-
-			var supportedCurrencies = ConfigurationManager.AppSettings["supported_currencies"].Split(';');
-			var currencies = (from node in nodes.Cast<XmlNode>()
-			                  let currencyName = node["EngName"]?.InnerText
-			                  where supportedCurrencies.Contains(currencyName)
-			                  let currencyId = node["ParentCode"]?.InnerText.Trim()
-							  select new Currency()
-			                         {
-				                         ID = currencyId,
-				                         Name = currencyName
-			                         }).ToList();
-
-			context.Currencies.AddRange(currencies);
-			context.SaveChanges();
-
-			return currencies;
-		}
-
-		private static void InitializeCurrencyRates(AppContext context, IEnumerable<Currency> currencies)
-		{
-			foreach (var currency in currencies)
-			{
-				const string date1 = "01/04/2016";
-				var date2 = DateTime.Now.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
-
-				var doc = new XmlDocument();
-				doc.Load($"http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1={date1}&date_req2={date2}&VAL_NM_RQ={currency.ID}");
-				var nodes = doc.SelectNodes("//Record");
-				if (null == nodes)
-				{
-					throw new XPathException();
-				}
-
-				context.CurrencyRates.AddRange(from node in nodes.Cast<XmlNode>()
-				                               let date = DateTime.Parse(node?.Attributes?["Date"].Value)
-				                               let nominal = int.Parse(node["Nominal"]?.InnerText ?? "1")
-				                               let value = node["Value"]?.InnerText
-				                               where null != value
-				                               select new CurrencyRate()
-				                                      {
-					                                      CurrencyID = currency.ID,
-					                                      Currency = currency,
-					                                      Date = date,
-					                                      Value = decimal.Parse(value)/nominal
-				                                      });
-			}
-
-			context.SaveChanges();
 		}
 	}
 }
